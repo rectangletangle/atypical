f6853a0f4a2d562e77e5a09fc8432a65aa705a8d
Drew French <rectangletangle@gmail.com>
2014-10-11 20:22:14 -0700

    added some test data

--- tests.py
+++ tests.py
@@ -1,6 +1,27 @@
 
+import random
+import os
+
 from . import atypical
 
 if __name__ == '__main__':
+    path = os.path.split(__file__)[0]
+    sample = os.path.join(path, 'data', 'sample.txt')
+
+    with open(sample) as f:
+        data = [word for word in f.read().lower().split() if len(word) > 3 and word.isalpha()]
+
+    extra = ['aaaaa', 'xxxxx', 'abcd', 'uuu']
+
+    data.extend(extra)
+
+    random.shuffle(data)
+
+    scored = [string for string, score in atypical(data)]
+
+    for string in extra:
+        print(string, scored.index(string))
+
     from pprint import pprint
-    pprint(atypical(['aab', 'bba', 'aabb', 'abb' 'ab', 'aaa', 'bbbb', 'cc', 'ac', 'zzzz']))
+
+    pprint(scored[:10])
